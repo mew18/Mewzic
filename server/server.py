0235a15fb228dc55cdd3d728f78572e729bfb9be
mew <33283117+mew18@users.noreply.github.com>
2020-08-20 20:42:29 +0530

    Minimized imports and function calls

--- server/server.py
+++ server/server.py
@@ -1,8 +1,9 @@
 from flask import Flask, request
-from flask.helpers import flash
-import get_req
+import transform
 import os
 from werkzeug.utils import secure_filename
+
+#  min the number of imports
 
 # tweak port settings
 # instance relative config allows u to change dir path VIMP
@@ -17,10 +18,8 @@ def upload_file():
 @server.route("/",methods=["POST", "GET"])
 # u know u can change routes to diff dir to exec diff function
 
-def get_mid():    
-
-    if request.method == 'POST':
-        
+def get_mid(): 
+    if request.method == 'POST':        
         f = request.files['file']
         
         server.instance_path = "./server/"
@@ -35,29 +34,11 @@ def get_mid():
         return 'midi uploaded successfully'
 
 # m8 optimize the shit out of this
-def pred():
-    print("Importing libraries ....")
-    from keras.models import load_model
-    import numpy as np
-
-    data=get_req.get_data()
-
-    # data = request.get_json()
-    
-    pred_ip = np.reshape(data, (1, len(data), 1))
-    pred_ip = pred_ip/len(pred_ip)
-
-    print("Predicting  ....")
-    model = load_model('./model/mewzic_model.h5')
-    prediction = model.predict(pred_ip)
-
-    #  process the data from midi into array and then feed into the model , maybe think of adding that convert file separetly
-
-    # print_tensor(prediction)
-    pred_op = np.around(prediction, 2)
-    pred_op = ' '.join(map(str, prediction))
-    return pred_op
+def pred():    
+    print(transform.pre_process())
+    return "Generated"
 
 if __name__ == "__main__":
     print("SERVER started in BackGround")
-    server.run(host="127.0.0.1",port=666,debug=True)
+    # server.run(host="127.0.0.1",port=666,debug=True)
+    pred()
