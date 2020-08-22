f33b1c810a9bdf280b62680f32056456e6925807
mew <33283117+mew18@users.noreply.github.com>
2020-08-22 20:13:21 +0530

    Minimized function and request calls

--- server/server.py
+++ server/server.py
@@ -10,35 +10,39 @@ from werkzeug.utils import secure_filename
 server = Flask(__name__, instance_relative_config=True)
 
 # coz template html does not server REST reqs
+
 @server.route('/')
 def upload_file():
-   return render_template('../client/app.html')
+    return render_template('../client/app.html')
 
-
-@server.route("/",methods=["POST", "GET"])
+@server.route("/get", methods=["POST", "GET"])
 # u know u can change routes to diff dir to exec diff function
-
-def get_mid(): 
-    if request.method == 'POST':        
+def get_mid():
+    if request.method == 'POST':
         f = request.files['file']
-        
+
         server.instance_path = "./server/"
-        uploads_dir = os.path.join(server.instance_path, 'user_upload_data')
+        uploads_dir = os.path.join(server.instance_path, 'user_data')
         os.makedirs(uploads_dir, exist_ok=True)
 
-        # f.filename = "user_ip.mid"
-        f.save(os.path.join(uploads_dir,secure_filename(f.filename)))
-        # f.save(f.filename)
+        f.save(os.path.join(uploads_dir, secure_filename(f.filename)))
+        # transform.generate()
 
         # think of going back to home page after this
+        # print(transform.generate())
         return 'midi uploaded successfully'
 
 # m8 optimize the shit out of this
-def pred():    
-    print(transform.pre_process())
-    return "Generated"
+
+
+@server.route("/predict", methods=["POST", "GET"])
+def predict():
+    if request.method == 'POST':
+        print("\n\n\nPREDICTING ........")
+        print(transform.generate())
+    return "GENERATED"
+
 
 if __name__ == "__main__":
     print("SERVER started in BackGround")
-    # server.run(host="127.0.0.1",port=666,debug=True)
-    pred()
+    server.run(host="127.0.0.1", port=666, debug=True)
