03fdb59bc7d50f079d096714917c5c1164ff2a3d
mew <33283117+mew18@users.noreply.github.com>
2020-08-27 23:56:28 +0530

    added redirects to og website

--- server/server.py
+++ server/server.py
@@ -1,22 +1,17 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash, redirect, url_for
 # import transform
 import os
-from werkzeug.utils import secure_filename
+from werkzeug.utils import redirect, secure_filename
 
-# tweak port settings
 # instance relative config allows u to change dir path VIMP
-server = Flask(__name__, instance_relative_config=True,template_folder='../client/')
+server = Flask(__name__, instance_relative_config=True,
+               template_folder='../client/')
 server.static_folder = '../client/'
-
-# coz template html does not server REST reqs
-
+server.secret_key = "joe mama"
 
 @server.route('/')
 def upload_file():
     return render_template('app.html')
-0
-# u know u can change routes to diff dir to exec diff function
-
 
 @server.route("/upload", methods=["POST", "GET"])
 def get_mid():
@@ -27,25 +22,25 @@ def get_mid():
             uploads_dir = os.path.join(server.instance_path, 'user_data')
             os.makedirs(uploads_dir, exist_ok=True)
             f.save(os.path.join(uploads_dir, secure_filename(f.filename)))
-            # return "midi uploaded successfully"
-            return render_template('app.html')        
+            # return redirect(url_for('pause'))
+            return render_template('app.html')
         except:
-            return "error"
-    return "Error"
-
+            return "Error in Uploading file, Please try again"
+            # return redirect(url_for('pause'))
+    # return render_template('app.html')
+    # return redirect(url_for('pause'))
+    return "Please select a valid midi file"
 
 @server.route("/predict", methods=["POST", "GET"])
 def predict():
     if request.method == 'POST':
         try:
-            print("\n\n\nPREDICTING ........")
             # print(transform.generate())
-            # return "GENERATED"
             return render_template('app.html')
         except:
-            return "error"
-    return "Error"
+            return "Error in generating music"
+    return "Error in the predict function "
 
 if __name__ == "__main__":
     print("SERVER started in BackGround")
-    server.run(host="127.0.0.1",port="5000",debug=True)
+    server.run(host="127.0.0.1", port="5000", debug=True)
