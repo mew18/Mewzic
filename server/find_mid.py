e461f9d9ac228edbce208940be6a8ab8efcef785
mew <33283117+mew18@users.noreply.github.com>
2021-01-12 21:54:52 +0530

    Delete find_mid.py

--- server/find_mid.py
+++
@@ -1,9 +0,0 @@
-def mid_exist():
-    from os import path,walk
-    dir_path = path.dirname(path.realpath(__file__))
-    for root, dirs, files in walk(dir_path):
-        for file in files:
-            if file.endswith('.mid'):
-                return True
-
-# print(mid_exist())
